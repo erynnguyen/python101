b891924e66ea1c3d1f5a603690a97e1a99e535cc
erynnguyen <trangnt2211@gmail.com>
2022-08-24 20:46:47 +0700

    Co-authored-by: alexngolx <quitribo@users.noreply.github.com>

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,10 @@
-from math import ceil
 
-
-print("hello worl")
-a %= (5/2)
-print(a)
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://google.com")
+print('driver Title:',driver.title)
+print('Driver name:',driver.name)
+print('Driver URL:',driver.current_url)
+driver.quit()
